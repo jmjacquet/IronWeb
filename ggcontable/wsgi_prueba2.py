9f0b9bc82fb626f0c731a03891fbc9c53135522c
jmjacquet <jmjacquet@gmail.com>
2019-03-19 15:06:55 -0300

    wsgi

---
+++ ggcontable/wsgi_prueba2.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
+
+sys.path.append(PROJECT_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ggcontable.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+try:
+	_application = None
+	def application(environ, start_response):
+		global _application
+		if _application == None:
+			os.environ['ENTIDAD_ID'] = 1
+			os.environ['ENTIDAD_DB'] = 'gg_copyfast'
+			os.environ['ENTIDAD_DIR'] = 'copyfast'
+			_application = get_wsgi_application()
+		return _application(environ, start_response)
+
+except Exception as e:
+    logger.error('Admin Command Error: %s', ' '.join(sys.argv), exc_info=sys.exc_info())
+    raise e	
