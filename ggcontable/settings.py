950d4b457a0ce8fb78b30f861d614abacd83b554
JuanMa <jmjacquet@gmail.com>
2019-07-29 08:21:09 -0300

    Update settings.py

--- ggcontable/settings.py
+++ ggcontable/settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django.contrib.admin',    
+    'django.contrib.admin',    
     'django.contrib.humanize',
     'bootstrap3',
     'crispy_forms',
