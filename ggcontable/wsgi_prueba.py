66f02f81c5849ca6637eba2984a62c798b29102f
JuanMa <jmjacquet@gmail.com>
2019-07-29 09:31:11 -0300

    wsgi

--- ggcontable/wsgi_prueba.py
+++ ggcontable/wsgi_prueba.py
@@ -6,7 +6,6 @@ PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
 
 sys.path.append(PROJECT_DIR)
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ggcontable.settings")
 os.environ['DJANGO_SETTINGS_MODULE'] = "ggcontable.development"
 os.environ['ENTIDAD_ID'] = '1'
 os.environ['ENTIDAD_DB'] = 'gg_ironweb'
