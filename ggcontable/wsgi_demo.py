7d8d9fa4aac60998f62bd95be81b2345b21d0ad5
jmjacquet <jmjacquet@gmail.com>
2019-09-27 19:31:26 -0300

    Update wsgi_demo.py

--- ggcontable/wsgi_demo.py
+++ ggcontable/wsgi_demo.py
@@ -10,6 +10,6 @@ sys.path.append(PROJECT_DIR)
 os.environ['DJANGO_SETTINGS_MODULE'] = "ggcontable.production"
 os.environ['ENTIDAD_ID'] = '1'
 os.environ['ENTIDAD_DB'] = 'ironweb_demo'
-os.environ['ENTIDAD_DIR'] = 'copyfast'
+os.environ['ENTIDAD_DIR'] = 'prueba'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
