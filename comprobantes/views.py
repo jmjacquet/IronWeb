53a448701c36033f9b693b8a97caf7737797f7d0
jmjacquet <jmjacquet@gmail.com>
2019-09-27 19:21:45 -0300

    Update views.py

--- comprobantes/views.py
+++ comprobantes/views.py
@@ -1208,7 +1208,7 @@ class MovInternosEditView(VariablesMixin,UpdateView):
         self.object = self.get_object()
         form_class = self.get_form_class()
         form = self.get_form(form_class)                       
-        form.fields['numero'].widget.attrs['disabled'] = True        
+        # form.fields['numero'].widget.attrs['disabled'] = True        
         CPBFPFormSet.form = staticmethod(curry(MovimCuentasFPForm,request=request))
         cpb_fp = CPBFPFormSet(instance=self.object,prefix='formFP')
         return self.render_to_response(self.get_context_data(form=form,cpb_fp = cpb_fp))
