ca5e2402f14e7caf4553d90299f5a47dc8ef8871
jmjacquet <jmjacquet@gmail.com>
2020-01-16 12:13:48 -0300

    Update utilidades.py

--- general/utilidades.py
+++ general/utilidades.py
@@ -562,3 +562,14 @@ MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
                 message_constants.SUCCESS: 'success',
                 message_constants.WARNING: 'warning',
                 message_constants.ERROR: 'danger',}
+
+
+def popover_html(label, content):
+    return label + ' &nbsp;<i class="fa fa-question-circle" data-toggle="tooltip" data-placement="top" title="'+ content +'"></i>'
+
+
+def agregar_nuevo_html(label,id,title,url,callback,hint,icon):
+    return label + '<a id="%s" href="%s" data-modal-head="%s" '\
+    'class="agregarDatos modal-create" type="button" data-toggle="tooltip" data-modal-callback="%s" '\
+    'data-placement="top" title="%s"><i class="%s"></i></a>'%(id,url,title,callback,hint,icon)
+                    
