35b038001d8affd66ce50e0ab636e018413210b2
jmjacquet <jmjacquet@gmail.com>
2020-05-27 19:33:48 -0300

    actaulizac

--- actualizar_iron_prueba.sh
+++
@@ -1,2 +0,0 @@
-sh /home/grupogua1/webapps/iron_prueba/apache2/bin/restart
-python /home/grupogua1/webapps/iron_prueba/ggcontable/manage.py collectstatic --noinput
--- actualizar_ironweb.sh
+++
@@ -1,2 +0,0 @@
-sh /home/grupogua1/webapps/ironweb/apache2/bin/restart
-python /home/grupogua1/webapps/ironweb/ggcontable/manage.py collectstatic --noinput
