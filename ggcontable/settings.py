5906b0d1f0dcd41715c3a6d7017316b4b833d728
Juanma <jmjacquet"gmail.com>
2020-11-18 20:35:09 -0300

    statics

--- ggcontable/settings.py
+++ ggcontable/settings.py
@@ -36,10 +36,9 @@ DEFAULT_CHARSET = 'utf-8'
 FILE_CHARSET = 'utf-8'
 TIME_INPUT_FORMATS = ('%H:%M',)
 DATE_INPUT_FORMATS = ('%d/%m/%Y',)
-MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
-MEDIA_URL = '/media/'
 
-# STATIC_ = '/home/grupogua1/webapps/ironweb/ggcontable/static'
+
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
